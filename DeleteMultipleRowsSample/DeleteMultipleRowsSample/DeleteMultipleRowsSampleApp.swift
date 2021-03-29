a2ddca34ae3667bc82b4e7e4e0e872a54b31f31f
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-03-29 20:02:30 +0900

    初回コミット

---
+++ DeleteMultipleRowsSample/DeleteMultipleRowsSample/DeleteMultipleRowsSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeleteMultipleRowsSampleApp.swift
+//  DeleteMultipleRowsSample
+//
+//  Created by home on 2021/03/29.
+//
+
+import SwiftUI
+
+@main
+struct DeleteMultipleRowsSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
